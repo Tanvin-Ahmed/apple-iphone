34e9fe75d52b36afdbe5b7d78967e3049943e23a
Tanvin-Ahmed <tanvinahmedtouhid91@gmail.com>
2024-03-26 11:53:20 +0600

    add features and howItWorks section in home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import HowItWorks from "./components/HowItWorks";
 import Model from "./components/Model";
 import Navbar from "./components/Navbar";
+import * as Sentry from "@sentry/react";
 
 const App = () => {
   return (
@@ -10,8 +13,10 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
+      <HowItWorks />
     </main>
   );
 };
 
-export default App;
+export default Sentry.withProfiler(App);
